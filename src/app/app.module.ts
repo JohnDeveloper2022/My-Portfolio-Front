3ac70b6f4b7a4df7f5794e0718d7673f1329463e
Johnny <jgamb.rod@gmail.com>
2022-11-15 19:42:56 -0300

    Maqueta de portfolio en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { IdiomasComponent } from './components/idiomas/idiomas.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    IdiomasComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
