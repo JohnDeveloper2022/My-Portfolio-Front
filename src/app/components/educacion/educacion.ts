2bd5af799da4991404f889044a5a41f7b584e17f
Johnny <jgamb.rod@gmail.com>
2023-03-22 16:39:37 -0300

    Servicio Educacion

---
+++ src/app/components/educacion/educacion.ts
@@ -0,0 +1,9 @@
+export class Educacion {
+    id:number;
+    nombre_instituto:string;
+    titulo:string;
+    fecha_inicio:number;
+    fecha_fin:number;
+    decripcion:string;
+    persona_id:number;
+}
