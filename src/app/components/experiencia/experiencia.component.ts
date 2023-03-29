34bd45db9ecb0cfd79228ca3f570565ce40cdc11
Johnny <jgamb.rod@gmail.com>
2023-03-29 10:56:43 -0300

    Inserción de formulario crear experiencia

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ExperienciaService } from 'src/app/service/experiencia.service';
+import { Experiencia } from './experiencia';
 
 @Component({
   selector: 'app-experiencia',
@@ -7,8 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienciaComponent implements OnInit {
 
-  constructor() { }
+  meses:string[];
+  yearList:number[] = [];
 
+  experiencia:Experiencia = new Experiencia();
+
+  constructor(private experienciaService:ExperienciaService, private router:Router, private activatedRoute:ActivatedRoute) { 
+
+    this.meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 
+    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+    for (let i = 1930; i <= 2023; i++) {
+      this.yearList.push(i);
+    }
+  }
+  
   ngOnInit(): void {
   }
 
