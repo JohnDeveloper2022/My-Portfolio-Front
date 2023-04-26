55e8fa2f67015d7f99abe365a56eff7863b0a332
Johnny <jgamb.rod@gmail.com>
2023-04-26 17:11:53 -0300

    Creación de función login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,42 +12,29 @@ export class LoginComponent implements OnInit {
 
   usuario:Usuario = new Usuario();
   userLogueado:boolean;
-  data:any;
+
   constructor(private loginService:LoginService, private router:Router, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
-  async login() {
-    try {
-      if(this.usuario.username && this.usuario.password) {
-        const data = await this.loginService.getUser(this.usuario);
-        if(data) {
-          alert('Usuario logueado con éxito');
-          this.router.navigate(['/edicion']);
-        }
-        else {
-          this.router.navigate(['/login']);
-        }
-      }
-    } catch (error) {
-      alert('Por favor ingrese sus datos correctamente');
-      this.router.navigate(['/login']);
-    }
+  /*login() {
+    this.loginService.getUser(this.usuario).subscribe(
+      data => {
+        alert('Usuario logueado con éxito');
+        this.router.navigate(['/edicion']);        
+    });
+  }*/
+  login() {
+    this.loginService.getUser(this.usuario).subscribe({
+      next: (data) => this.router.navigate(['/edicion']),
+      error: (e) => alert('Por favor ingrese sus datos correctamente'),
+      complete: () => alert('Usuario logueado con éxito')
+    });
   }
 
   volver(): void {
     this.router.navigate(['']);
   }
-
-  /*this.loginService.getUser(this.usuario).subscribe(data=> { 
-      this.router.navigate(['/edicion'])
-    });
-    if (this.userLogueado) {
-      window.alert('Usuario logueado con éxito');
-    }
-    else {
-      window.alert('Por favor ingrese sus datos correctamente');
-    }*/
 
 }
